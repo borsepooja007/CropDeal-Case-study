57630200366ec210f79dc2e7db73b4beeaeb363e
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-06-07 22:54:54 +0530

    AdminService

---
+++ Admin_serviceMain/Admin_serviceMain/src/main/java/Admin_Service/Admin_serviceMain/AdminServiceMainApplication.java
@@ -0,0 +1,13 @@
+package Admin_Service.Admin_serviceMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceMainApplication.class, args);
+	}
+
+}
