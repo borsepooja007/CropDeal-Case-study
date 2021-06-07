09862cda84bb05b9f369fe73bfcadd0b998638fe
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-06-07 22:57:44 +0530

    EurekaFarmerLogin

---
+++ Farmer_ServiceMain/Farmer_ServiceMain/src/main/java/Farmer_Service/Farmer_ServiceMain/FarmerServiceMainApplication.java
@@ -0,0 +1,14 @@
+package Farmer_Service.Farmer_ServiceMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerServiceMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerServiceMainApplication.class, args);
+
+	}
+
+}
