e4b3101b59583896fe09ca9936d24e889f40cbc6
borsepooja007 <59304709+borsepooja007@users.noreply.github.com>
2021-06-07 22:56:52 +0530

    DealerService

---
+++ Dealer_ServiceMain/Dealer_ServiceMain/src/main/java/Dealer_Service/Dealer_ServiceMain/DealerServiceMainApplication.java
@@ -0,0 +1,13 @@
+package Dealer_Service.Dealer_ServiceMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerServiceMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerServiceMainApplication.class, args);
+	}
+
+}
